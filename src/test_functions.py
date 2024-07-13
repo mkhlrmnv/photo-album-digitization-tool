b5bd6ec7815b71b1d691b326188e479e3eac3906
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-07-13 17:04:28 +0300

    saved all test images into test_images folder so test doesn't need to create them each time

--- src/test_functions.py
+++ src/test_functions.py
@@ -12,8 +12,7 @@ class TestFunctions(unittest.TestCase):
         cv2.rectangle(image, (20, 20), (80, 80), (0, 0, 0), -1)
 
         # Save the test image to a temporary file
-        temp_file = 'test_image.png'
-        cv2.imwrite(temp_file, image)
+        temp_file = 'test_pictures/test_remove_with_contours.png'
 
         # Call the remove_white function
         result = remove_white(temp_file)
@@ -22,8 +21,6 @@ class TestFunctions(unittest.TestCase):
         expected_result = image[20:80, 20:80]
         self.assertTrue(np.array_equal(result, expected_result))
 
-        # Clean up the temporary file
-        os.remove(temp_file)
 
     def test_remove_white_without_contour(self):
         # Create a test image with a white background
@@ -31,8 +28,7 @@ class TestFunctions(unittest.TestCase):
         image.fill(255)
 
         # Save the test image to a temporary file
-        temp_file = 'test_image.png'
-        cv2.imwrite(temp_file, image)
+        temp_file = 'test_pictures/test_remove_white_without_contours.png'
 
         # Call the remove_white function
         result = remove_white(temp_file)
@@ -40,8 +36,6 @@ class TestFunctions(unittest.TestCase):
         # Assert that the result is the original image
         self.assertTrue(np.array_equal(result, image))
 
-        # Clean up the temporary file
-        os.remove(temp_file)
 
     def test_crop_in_four_pieces(self):
         image = np.zeros((100, 100, 3), dtype=np.uint8)
@@ -50,13 +44,10 @@ class TestFunctions(unittest.TestCase):
         cv2.rectangle(image, (0, 50), (50, 100), (255, 0, 0), -1)
         cv2.rectangle(image, (50, 50), (100, 100), (0, 255, 0), -1)
 
-        cv2.imshow("orig", image)
-
-        temp_file = 'test_image.png'
-        cv2.imwrite(temp_file, image)
+        temp_file = 'test_pictures/test_crop_in_four.png'
 
         # Call the crop_in_four_pieces function
-        result = crop_in_four_pieces('test_image.png')
+        result = crop_in_four_pieces('test_pictures/test_crop_in_four.png')
 
         # Assert that the result is a list containing four cropped image pieces
         self.assertEqual(len(result), 4)
@@ -65,6 +56,12 @@ class TestFunctions(unittest.TestCase):
                 self.assertTrue(isinstance(result[i * 2 + j], np.ndarray))
                 self.assertEqual(result[i * 2 + j].shape, (50, 50, 3))
                 self.assertTrue(np.array_equal(result[i * 2 + j], image[j * 50:(j + 1) * 50, i * 50:(i + 1) * 50]))
+
+    def test_get_picture(self):
+        for f in os.listdir('test_pictures/test_get_pictures'):
+            if f.endswith(".jpg") or f.endswith(".jpeg"):
+                path = os.path.join('input', f)
+                self.assertEqual(2, len(get_pictures(path)))
 
 if __name__ == '__main__':
     unittest.main()
